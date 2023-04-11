18a2ea5336568bb6fbbeb0028948dfcfffd0a9ef
Deepshi Sharma <deepshisharma123@gmail.com>
2023-04-11 17:17:11 +0530

    Implemented sorting of records according to columns

--- mini-music-library/src/components/MusicIndex.vue
+++ mini-music-library/src/components/MusicIndex.vue
@@ -4,22 +4,29 @@
     class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
   >
     <thead>
-      <th style="30%">Title</th>
-      <th style="30%">Artist</th>
-      <th style="20%">Album Type</th>
-      <th style="20%">Release Date</th>
+      <th style="30%"><a @click="change_sort('name')">Name
+        <fa-icon icon="chevron-up" v-if="sortBy == 'name' && sortDirection == 'asc'" />
+        <fa-icon icon="chevron-down" v-if="sortBy == 'name' && sortDirection == 'desc'" /></a></th>
+      <th style="30%"><a @click="change_sort('artist')">Artist
+        <fa-icon icon="chevron-up" v-if="sortBy == 'artist' && sortDirection == 'asc'" />
+        <fa-icon icon="chevron-down" v-if="sortBy == 'artist' && sortDirection == 'desc'" /></a></th>
+      <th style="20%"><a @click="change_sort('album_type')">Album Type
+        <fa-icon icon="chevron-up" v-if="sortBy == 'album_type' && sortDirection == 'asc'" />
+        <fa-icon icon="chevron-down" v-if="sortBy == 'album_type' && sortDirection == 'desc'" /></a></th>
+      <th style="20%"><a @click="change_sort('release_date')">Release Date
+        <fa-icon icon="chevron-up" v-if="sortBy == 'release_date' && sortDirection == 'asc'" />
+        <fa-icon icon="chevron-down" v-if="sortBy == 'release_date' && sortDirection == 'desc'" /></a></th>
     </thead>
-    <tbody v-for="(song, index) in songs" :key="index">
+    <tbody v-for="(song, index) in sortedList" :key="index">
       <tr>
-        <td>{{ song.track.name }}</td>
-        <td>{{ song.track.artists[0].name }}</td>
-        <td>{{ song.track.album.album_type }}</td>
-        <td>{{ song.track.album.release_date }}</td>
+        <td>{{ song.name }}</td>
+        <td>{{ song.artist }}</td>
+        <td>{{ song.album_type }}</td>
+        <td>{{ song.release_date }}</td>
       </tr>
     </tbody>
   </table>
 </template>
-/* eslint-disable */
 <script>
 import MusicData from '@/assets/list.json'
 export default {
@@ -28,6 +35,37 @@ export default {
       songs: MusicData,
       sortBy: '',
       sortDirection: 'asc'
+    }
+  },
+  computed: {
+    sortedList () {
+      if (this.sortBy === '') {
+        return this.songs
+      }
+      let sortModifier = this.sortDirection === 'asc' ? 1 : -1
+      return this.songs.slice().sort((a, b) => {
+        console.log('a is' + a)
+        let columnA = a[this.sortBy].toUpperCase()
+        let columnB = b[this.sortBy].toUpperCase()
+        console.log(columnA + ' ' + columnB)
+        if (columnA > columnB) {
+          return 1 * sortModifier
+        } else if (columnA === columnB) {
+          return 0
+        } else {
+          return -1 * sortModifier
+        }
+      })
+    }
+  },
+  methods: {
+    change_sort (column) {
+      if (this.sortBy === column && this.sortDirection === 'asc') {
+        this.sortDirection = 'desc'
+      } else {
+        this.sortDirection = 'asc'
+      }
+      this.sortBy = column
     }
   }
 }
